f0507435effdaa2575f018c8108e6eca7fdf9f09
Martin T <mtitus575@gmail.com>
2025-03-10 19:03:33 +0000

    Created a function to update the score for each round

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+/*  This function creates a random number between 0 and 9.
+    It will be called at the start of each new round to create a new secret number */
 const generateTarget = () => {
     return Math.floor(Math.random() * 10);
 };
 
+/*  This function is used to compare the user and computer guesses. It checks to see which 
+    guess is closer to the secret number */
 const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
     const humanDifference = Math.abs(humanGuess - targetGuess);
     const computerDifference = Math.abs(computerGuess - targetGuess);
@@ -19,6 +22,15 @@ const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
     }
 };
 
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++
+    } else if (winner === 'computer') {
+        computerScore++
+    } else {
+        'An unexpected error occured'
+    }
+};
 
 
 
