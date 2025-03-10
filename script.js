6e2bee633d42ecc26d6ab998716e49b7af80c245
Martin T <mtitus575@gmail.com>
2025-03-10 18:51:59 +0000

    Functions for random target number and to compare the guesses

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+};
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(humanGuess - targetGuess);
+    const computerDifference = Math.abs(computerGuess - targetGuess);
+
+    if (humanDifference <= computerDifference) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+
+
+
